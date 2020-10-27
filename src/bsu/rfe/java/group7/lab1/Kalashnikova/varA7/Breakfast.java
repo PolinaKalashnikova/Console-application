00ac22c2e317ee46a96bd9cf86eb1749da6e4ccb
PolinaKalashnikova <polina.kalasnnikova.2002@bk.ru>
2020-10-27 19:48:36 +0300

    Реализована процедура подсчѐта в завтраке продуктов заданного типа в классе Breakfast

--- src/bsu/rfe/java/group7/lab1/Kalashnikova/varA7/Breakfast.java
+++ src/bsu/rfe/java/group7/lab1/Kalashnikova/varA7/Breakfast.java
@@ -2,6 +2,20 @@ package bsu.rfe.java.group7.lab1.Kalashnikova.varA7;
 
 public class Breakfast {
     public static void main(String[] args) {
-
+        int sum = 0;
+        int i = 0;
+        for (String arg1 : args) {
+            String[] part = arg1.split("/");
+            if (part[0].equals("Ice_Cream")) {
+                Ice_Cream i_c = new Ice_Cream(part[1]);
+                i_c.consume();
+                sum++;}
+            else if (part[0].equals("Cheese")){
+                Cheese cheese = new Cheese();
+                cheese.consume();
+                sum++;}
+            i++;
+        }
+        System.out.println("Количество съеденных продуктов:  " + sum);
     }
 }
